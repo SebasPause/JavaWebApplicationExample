4a6588585b9f61d18e7e5724870d7c92229acda5
Sebastian <sebastianpauseniuc@gmail.com>
2025-11-21 23:44:21 +0100

    Enviar datos entre servlets y volver al inicio

--- src/main/java/ejemplos/servlet/curso/MyServlet.java
+++ src/main/java/ejemplos/servlet/curso/MyServlet.java
@@ -1,6 +1,5 @@
 package ejemplos.servlet.curso;
 
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -12,18 +11,40 @@ import java.io.PrintWriter;
 public class MyServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException {
+			HttpServletResponse response) throws IOException {
 
 		response.setContentType("text/html");
 		PrintWriter out = response.getWriter();
 
-		// send HTML page to client
+        // INICIO
 		out.println("<html>");
-		out.println("<head><title>Ejemplo HTML desde Servlet</title></head>");
+		out.println("<head><title>Formulario</title></head>");
+
+        // ESTILOS
+        out.println("<style>");
+        out.println("body {");
+        out.println("  display: flex;text-align:center;justify-content: center;align-items: center;");
+        out.println("  height: 100vh;margin: 0;");
+        out.println("}");
+        out.println(".contenedor {");
+        out.println("  border: 2px solid black;padding: 30px;text-align: center;");
+        out.println("  width: 300px;border-radius: 10px;");
+        out.println("}");
+        out.println("</style>");
+        out.println("</head>");
+
+        // BODY
 		out.println("<body>");
-		out.println("<h1>Ejemplo Servlet</h1>");
-		out.println("<p>Este es un ejemplo en el curso de Java para generar HTML desde un Servlet.</p>");
-		out.println("<p><a href=\"/myServlet2\">Vamos al otro Servlet</a></p>");
+        out.println("<div class='contenedor'>");
+		out.println("<h1>Login</h1>");
+
+        // FORMULARIO
+        out.println("<form action='/myServlet2' method='GET'>");
+        out.println("Nombre: <input type='text' name='nombre' required><br><br>");
+        out.println("<input type='submit' value='Iniciar sesión'>");
+        out.println("</form>");
+
+        out.println("</div>");
 		out.println("</body></html>");
 	}
 
--- src/main/java/ejemplos/servlet/curso/MyServlet2.java
+++ src/main/java/ejemplos/servlet/curso/MyServlet2.java
@@ -1,6 +1,5 @@
 package ejemplos.servlet.curso;
 
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -12,16 +11,41 @@ import java.io.PrintWriter;
 public class MyServlet2 extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException {
+			HttpServletResponse response) throws IOException {
 
 		response.setContentType("text/html");
 		PrintWriter out = response.getWriter();
 
+        // Obtener datos del formulario
+        String nombre = request.getParameter("nombre");
+
 		// send HTML page to client
 		out.println("<html>");
-		out.println("<head><title>Ejemplo HTML desde Servlet</title></head>");
+		out.println("<head><title>Formulario</title></head>");
+
+        // ESTILOS
+        out.println("<style>");
+        out.println("body {");
+        out.println("  display: flex;text-align:center;justify-content: center;align-items: center;");
+        out.println("  height: 100vh;margin: 0;");
+        out.println("}");
+        out.println(".contenedor {");
+        out.println("  border: 2px solid black;padding: 30px;text-align: center;");
+        out.println("  width: 300px;border-radius: 10px;");
+        out.println("}");
+        out.println("</style>");
+        out.println("</head>");
+
 		out.println("<body>");
-		out.println("<h1>Bienvenido!!</h1>");
+        out.println("<div class='contenedor'>");
+		out.println("<h1>Bienvenid@ " + nombre + "!!</h1>");
+
+        out.println("<form action='/myServlet' method='GET'>");
+        out.println("<input type='submit' value='Cerrar sesión' " +
+                "style='padding:10px 20px; font-size:16px;'>");
+        out.println("</form>");
+
+        out.println("</div>");
 	}
 
 }
